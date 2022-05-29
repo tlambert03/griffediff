b8d27369dc97dd70c5326a8a1e1481a236ec791f
Talley Lambert <talley.lambert@gmail.com>
2022-05-29 12:33:29 -0400

    test: include mock git repo (#3)
    * test: include mock git repo
    
    * test: soften test
    
    * fix: remove include_extras kwarg
    
    * test: remove unnecessary stuff from mock git repo

--- tests/test_gitgriffe.py
+++ tests/test_gitgriffe.py
@@ -1,6 +1,25 @@
+import os
+import shutil
+from pathlib import Path
+
+import pytest
+
 from griffediff._git import git_compare
 
+API_PKG = Path(__file__).parent / "api-pkg"
 
-def test_compare():
-    aa = git_compare("v0.3.0", "v0.3.4", "psygnal", "/Users/talley/python/psygnal")
-    assert list(aa)
+
+@pytest.fixture
+def api_pkg(tmp_path: Path):
+    new_dir = shutil.copytree(API_PKG, tmp_path / "api-pkg")
+    os.rename((new_dir / "_git"), (new_dir / ".git"))
+    return new_dir
+
+
+def test_git_compare(api_pkg: Path):
+    aa = list(git_compare("old", "new", "api", api_pkg))
+    assert len(aa) > 16
+    msgs = {str(i) for i in aa}
+    assert "Parameter removed: api.change_param_name ('b')" in msgs
+    assert "New required parameter added: api.change_param_name ('c')" in msgs
+    assert "Parameter now required: api.change_kwarg_to_pos ('b')" in msgs
